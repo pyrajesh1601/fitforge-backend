6a4f60ee3ec3318220ff979a7995998c018448c2
pyrajesh1601 <py.rajesh16@gmail.com>
2026-07-01 10:56:06 +0530

    initia Spring Boot project setup

---
+++ src/main/java/com/fitforge/fitforge_ai/FitforgeAiApplication.java
@@ -0,0 +1,13 @@
+package com.fitforge.fitforge_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitforgeAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitforgeAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fitforge/fitforge_ai/FitforgeAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitforge.fitforge_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitforgeAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
